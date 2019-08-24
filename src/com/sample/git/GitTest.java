c6d5cbdea8acb80f39061e6e7eda2d89df68a133
Manjeera123 <manjeera.miriyam@yahoo.co.in>
2019-08-24 11:42:48 +0530

    deleted statement

--- src/com/sample/git/GitTest.java
+++ src/com/sample/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("created code by RK");
 		System.out.println("updated code by srilatha");
-		System.out.println("second time updated by RK");
+		
 	}
 
 }
