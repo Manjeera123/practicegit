35cb9aac0b3609c2ff35025c9f78a45f6eff90cd
Manjeera123 <manjeera.miriyam@yahoo.co.in>
2019-08-24 11:34:51 +0530

    updted by srilatha

--- src/com/sample/git/GitTest.java
+++ src/com/sample/git/GitTest.java
@@ -3,6 +3,7 @@ package com.sample.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("created code by RK");
+		System.out.println("updated code by srilatha");
 	}
 
 }
